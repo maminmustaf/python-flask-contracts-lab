6d14dfb6e1087711b67c8a8fbcd92eaaf86b192d
maminmustaf <mustafmohamedamin000@gmail.com>
2026-09-22 15:16:31 +0300

    Complete Flask contracts lab

--- server/app.py
+++ server/app.py
@@ -6,5 +6,20 @@ contracts = [{"id": 1, "contract_information": "This contract is for John and bu
 customers = ["bob","bill","john","sarah"]
 app = Flask(__name__)
 
+@app.route("/contract/<int:id>")
+def contract(id):
+    for contract in contracts:
+        if contract["id"] == id:
+            return contract["contract_information"], 200
+    return "Contract not found", 404
+
+
+@app.route("/customer/<customer_name>")
+def customer(customer_name):
+    if customer_name in customers:
+        return "", 204
+    return "Customer not found", 404
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
